591379678ed654d5e4a2753858fd37a632be6f6a
mahankhalili <61711582+mahankhalili@users.noreply.github.com>
2021-01-29 09:49:13 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Slaughterhousene
+
+Developed with Unreal Engine 4
---
+++ Source/Slaughterhousene/SlaughterhouseneGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SlaughterhouseneGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SLAUGHTERHOUSENE_API ASlaughterhouseneGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
